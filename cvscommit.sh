a2627f7772b25cbcf10f11f672af290b1d13a1a3
Wakaba <w@suika.fam.cx>
2012-01-14 16:21:50 +0900

    Removed CVS things

--- cvscommit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-find -name ChangeLog | xargs cvs diff | grep "^\+" | sed -e "s/^\+//; s/^\+\+ .\//++ wtt\//" > .cvslog.tmp
-cvs commit -F .cvslog.tmp $1 $2 $3 $4 $5 $6 $7 $8 $9 
-rm .cvslog.tmp
-
-## $Date: 2008/10/09 11:34:12 $
-## License: Public Domain
